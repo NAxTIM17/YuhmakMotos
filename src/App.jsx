a7c363f31aed4c2c6e0717f9d9c880ba193cc326
NAxTIM17 <naxtim14@gmail.com>
2024-05-31 11:07:09 -0300

    feat: div for bike information was created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,74 @@
-import './App.css'
-import { Canvas, useFrame, useThree, useLoader,  } from '@react-three/fiber'
+import "./App.css";
+import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
 //import { OrbitControls } from 'three/examples/jsm/Addons.js'
-import { GLTFLoader } from 'three/examples/jsm/Addons.js'
-import { Environment, ContactShadows, Fisheye,  PerspectiveCamera, OrbitControls  } from '@react-three/drei'
-import { Suspense, useEffect, useRef } from 'react'
-
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import {
+  Environment,
+  ContactShadows,
+  Fisheye,
+  PerspectiveCamera,
+  OrbitControls,
+} from "@react-three/drei";
+import { Suspense, useEffect, useRef } from "react";
 
 export default function App() {
-
   const CameraController = () => {
     //desestructura de useThree la camara y el gl
-    const {camera, gl} = useThree();
+    const { camera, gl } = useThree();
 
-    useEffect(()=>{
+    useEffect(() => {
       // crea el contructor de la orbita el contructor recibe dos parametros, la camara y el elemento
-      const controls = new OrbitControls(camera, gl.domElement)
+      const controls = new OrbitControls(camera, gl.domElement);
       // se fijan las distancias de zoom
       controls.minDistance = 3;
       controls.maxDistance = 15;
       controls.minPolarAngle = 0;
-      controls.maxPolarAngle = Math.PI / 1.90
-      controls.enablePan= false
+      controls.maxPolarAngle = Math.PI / 1.9;
+      controls.enablePan = false;
       //lo que entindo aqui es que limpa los controles
       return () => {
-        controls.dispose()
-      }
+        controls.dispose();
+      };
+    }, [camera, gl]);
 
-    },[camera, gl])
-
-    return null
-  }
+    return null;
+  };
   const RotateMyBox = () => {
     const myRef = useRef();
-    useFrame(({clock}) => {
-      myRef.current.rotation.y = clock.getElapsedTime()
-    })
-    return(
+    useFrame(({ clock }) => {
+      myRef.current.rotation.y = clock.getElapsedTime();
+    });
+    return (
       <mesh ref={myRef}>
-        <boxGeometry args={[2,2,2]} />
-        <meshStandardMaterial color={'white'} emissive={"red"} />
+        <boxGeometry args={[2, 2, 2]} />
+        <meshStandardMaterial color={"white"} emissive={"red"} />
       </mesh>
-    )
-  }
+    );
+  };
   const BikeModel = () => {
-    const gltf = useLoader(GLTFLoader, './public/kawashaki_ninja_h2/scene.gltf')
-    return(
+    const gltf = useLoader(
+      GLTFLoader,
+      "./public/kawashaki_ninja_h2/scene.gltf"
+    );
+    return (
       <>
-        <primitive object={gltf.scene} scale={2.5} position={[0,-1.50,0]}/>
+        <primitive object={gltf.scene} scale={2.5} position={[0, -1.5, 0]} />
       </>
-    )
-  }
+    );
+  };
 
   return (
     <>
-    <div className='motos__header'>
-      <h1>Yuhmak</h1>
-      <div className='header__line'>
+      <div className="motos__header">
+        <h1>Yuhmak</h1>
+        <div className="header__line"></div>
       </div>
-    </div>
-        <div className='moto__container'>
-          <div className='three__canvas'>
-              <Canvas>
+      <div className="moto__container">
+        <h1 className="moto__container--title">Ninja H2</h1>
+          <div className="three__canvas">
+            <Canvas>
                 <Suspense fallback={null}>  
-                <Fisheye resolution={4086} zoom={3}>
+                <Fisheye resolution={2048} zoom={4}>
                   <Environment files="./public/dresden_station_night_4k.hdr" background={false} />  
                   <color attach={'background'} args={['black']}/>       
                   <BikeModel/>
@@ -70,15 +76,11 @@ export default function App() {
                   <PerspectiveCamera makeDefault position={[0, 5, -10]} fov={100} />
                 </Fisheye>   
                 </Suspense>
-              </Canvas>
+              </Canvas> 
           </div>
-          <div className='moto__info'>
-              <h1>Ninja H2</h1>
-              <div className='info__container'>
-                
-              </div>
-          </div>
-        </div>
+          <div className="moto__info"></div>
+          <div className="info__container"></div>
+      </div>
     </>
-  )
+  );
 }
