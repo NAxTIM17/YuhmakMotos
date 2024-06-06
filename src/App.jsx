1db6d6ebd52b3fffcd6227df6a6fba0c41afc6f2
NAxTIM17 <naxtim14@gmail.com>
2024-06-05 22:30:12 -0300

    feat: now several models can be displayed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,67 @@
 import "./App.css";
-import { Canvas, useFrame, useThree, useLoader } from "@react-three/fiber";
-//import { OrbitControls } from 'three/examples/jsm/Addons.js'
+import { Canvas, addEffect, useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import {
   Environment,
-  ContactShadows,
-  Fisheye,
   PerspectiveCamera,
   OrbitControls,
+  View,
+  Fisheye,
 } from "@react-three/drei";
-import { Suspense, useEffect, useRef } from "react";
+import { useEffect } from "react";
+import { ReactLenis, useLenis } from 'lenis/react'
+import Lenis from "lenis";
 
 export default function App() {
-  const CameraController = () => {
-    //desestructura de useThree la camara y el gl
-    const { camera, gl } = useThree();
 
-    useEffect(() => {
-      // crea el contructor de la orbita el contructor recibe dos parametros, la camara y el elemento
-      const controls = new OrbitControls(camera, gl.domElement);
-      // se fijan las distancias de zoom
-      controls.minDistance = 3;
-      controls.maxDistance = 15;
-      controls.minPolarAngle = 0;
-      controls.maxPolarAngle = Math.PI / 1.9;
-      controls.enablePan = false;
-      //lo que entindo aqui es que limpa los controles
-      return () => {
-        controls.dispose();
-      };
-    }, [camera, gl]);
-
-    return null;
-  };
-  const RotateMyBox = () => {
-    const myRef = useRef();
-    useFrame(({ clock }) => {
-      myRef.current.rotation.y = clock.getElapsedTime();
-    });
-    return (
-      <mesh ref={myRef}>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color={"white"} emissive={"red"} />
-      </mesh>
-    );
-  };
-  const BikeModel = () => {
-    const gltf = useLoader(
+  const KawasakiNinjaH2 = () => {
+    const { scene } = useLoader(
       GLTFLoader,
-      "./public/kawashaki_ninja_h2/scene.gltf"
+      "./public/kawashaki_ninja_h2/KawasakiH2.gltf"
     );
     return (
       <>
-        <primitive object={gltf.scene} scale={2.5} position={[0, -1.5, 0]} />
+        <primitive object={scene} scale={2} position={[0, -1.5, 0]} />
       </>
     );
   };
+  const KawasakiZ1000 = () => {
+    const { scene } = useLoader(
+      GLTFLoader,
+      "./public/Kawasaki_z1000/KawsakiZ1000_03.gltf"
+    );
+    return (
+      <>
+        <primitive object={scene} scale={2} position={[0, -1.5, 0]} />
+      </>
+    );
+  } 
+  const SetUp = () => {
+    return (
+      <>
+        <Environment
+          files="./public/dresden_station_night_4k.hdr"
+          background={false}
+        />
+        <PerspectiveCamera makeDefault fov={50} position={[0, 0, 5]} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={false}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 1.8}
+          makeDefault
+          autoRotate
+        />
+      </>
+    );
+  };
+
+  // const lenis = new Lenis({syncTouch: true})
+  // addEffect((t) => lenis.raf(t))
+
+  useEffect(() => {
+    console.log(document.getElementById("moto"));
+  }, []);
 
   return (
     <>
@@ -63,23 +69,35 @@ export default function App() {
         <h1>Yuhmak</h1>
         <div className="header__line"></div>
       </div>
-      <div className="moto__container">
-        <h1 className="moto__container--title">Ninja H2</h1>
+      <div className="moto__body" id="moto">
+        <div className="moto__container">
+          <h1 className="moto__container--title">Ninja H2</h1>
           <div className="three__canvas">
-            <Canvas>
-                <Suspense fallback={null}>  
-                <Fisheye resolution={2048} zoom={4}>
-                  <Environment files="./public/dresden_station_night_4k.hdr" background={false} />  
-                  <color attach={'background'} args={['black']}/>       
-                  <BikeModel/>
-                  <OrbitControls enableZoom={true} enablePan={false} minPolarAngle={0} maxPolarAngle={Math.PI / 1.80} makeDefault autoRotate />
-                  <PerspectiveCamera makeDefault position={[0, 5, -10]} fov={100} />
-                </Fisheye>   
-                </Suspense>
-              </Canvas> 
+            <View className="view__bike">
+                <SetUp/>
+                <KawasakiNinjaH2/>
+            </View>
           </div>
-          <div className="moto__info"></div>
+           <div className="moto__info"></div>
           <div className="info__container"></div>
+        </div>
+        <div className="moto__container">
+          <h1 className="moto__container--title">Z 1000</h1>
+          <div className="three__canvas">
+            <View className="view__bike">
+              <SetUp/>
+              <KawasakiZ1000/>
+            </View>
+          </div>
+          {/* <div className="moto__info"></div>
+          <div className="info__container"></div> */}
+        </div>
+        <Canvas
+          className="motos__canvas"
+          eventSource={document.getElementById("moto")}
+        >
+          <View.Port />
+        </Canvas>
       </div>
     </>
   );
